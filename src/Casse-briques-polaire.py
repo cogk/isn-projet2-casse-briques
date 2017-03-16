61c60aaebb3b4dbcc16b14b37a67223b62d9dc7c
Corentin Forler [cogk42] <corentin.forler@gmail.com>
2017-03-16 20:17:32 +0100

    Coordonnées polaires implémentées.

--- src/Casse-briques-polaire.py
+++ src/Casse-briques-polaire.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-# coding=utf-8
+# -*- coding: utf-8 -*-
 
 from math import sin, cos
 from tkinter import *
-import time
 from random import random
+from math import pi as PI
+import time
 
 # import PIL
 # from PIL import Image, ImageTk
@@ -120,13 +121,13 @@ balle_x_initial = largeur_jeu / 2
 balle_y_initial = hauteur_jeu - 100
 
 # vitesse en pixels par image (*frame* en anglais)
-balle_vitesse_y_initiale = 0.5
-balle_vitesse_x = random() / 10
-balle_vitesse_y = balle_vitesse_y_initiale
+# balle_vitesse_y_initiale = 0.1
+# balle_vitesse_x = random() / 10
+# balle_vitesse_y = balle_vitesse_y_initiale
 
 # Vecteur vitesse en coordonnées polaires.
-# balle_vitesse = .4
-# balle_direction = 0 # 0° = vers la droite
+balle_vitesse = .5
+balle_direction = 45 # 0° = vers la droite, comme pour le cercle trigonométrique standard
 
 
 balle = fond.create_oval(balle_x_initial, balle_y_initial,
@@ -214,7 +215,8 @@ def déplacer_barre(event):
 
 
 def déplacer_balle(dt):
-    global balle_vitesse_x, balle_vitesse_y, joueur_vies_restantes
+    # global balle_vitesse_x, balle_vitesse_y, joueur_vies_restantes
+    global balle_direction, joueur_vies_restantes
     x, y, y2, x2 = fond.coords(balle)
 
     # Gestion des rebonds avec la barre
@@ -230,10 +232,11 @@ def déplacer_balle(dt):
     # - si la balle a une ordonnée entre y1 et y2
 
     if (barre_x1 - balle_rayon/2 < x < barre_x2 + balle_rayon/2) and (barre_y1 - balle_rayon/2 < y < barre_y2):
-        balle_vitesse_y = -balle_vitesse_y
+        # balle_vitesse_y = -balle_vitesse_y
         # y = barre_y1 - balle_rayon/2
 
-    # S'il y a collision avec la barre, alors en toute logique il n'y a pas de collision avec le cadre du jeu.
+        balle_direction = 180 - balle_direction
+
 
 
     # Gestion des rebonds avec le cadre
@@ -242,29 +245,29 @@ def déplacer_balle(dt):
     # En effet, si elle sort du cadre, alors le vecteur vitesse peut la forcer à rester en dehors.
 
     if x < balle_rayon/2:
-        balle_vitesse_x = -balle_vitesse_x
+        balle_direction = -balle_direction
         x = balle_rayon/2
 
     if x > largeur_jeu - balle_rayon/2:
-        balle_vitesse_x = -balle_vitesse_x
+        balle_direction = -balle_direction
         x = largeur_jeu - balle_rayon/2
 
     if y < balle_rayon/2:
-        balle_vitesse_y = - balle_vitesse_y
+        balle_direction = 180 - balle_direction # π - θ
         y = balle_rayon/2
 
     if y > hauteur_jeu - balle_rayon/2:
-        balle_vitesse_y = -balle_vitesse_y
+        balle_direction = 180 - balle_direction
         y = hauteur_jeu - balle_rayon/2
 
+
+        # À DÉPLACER
         marche_arrêt() # pause
-
         joueur_vies_restantes -= 1 # le joueur perd une vie
+        balle_direction = (random() - 0.5) * 90
+        print(balle_direction)
 
-        balle_vitesse_x = 0
-        balle_vitesse_y = balle_vitesse_y_initiale
         x, y = balle_x_initial, balle_y_initial
-
 
         if joueur_vies_restantes == 0:
             # message=fond.create_text('oooo')
@@ -290,8 +293,24 @@ def déplacer_balle(dt):
 
     # Application du vecteur vitesse
     # Pas d'accélération ici, on ne se place pas dans des conditions réelles (pas de gravité)
-    x2, y2 = x + balle_vitesse_x * dt, y + balle_vitesse_y * dt # ici, le dt sert à conserver une animation fluide (en théorie)
-    fond.coords(balle, (x2, y2, x2, y2)) # on déplace la balle
+    # Ici, le dt sert à conserver une animation fluide (en théorie)
+
+    # balle_direction = balle_direction % 360
+
+
+    # Conversion degrés -> radians : α = θ * 2π/360° = θ * π/180°
+    # Transformation pour que l'angle corresponde à la mesure principale
+    # dans le cercle trigonométrique: β = -α + π/2
+    # On obtient les coordonnées du vecteur selon :
+    # v_x = ||v⃗|| × cos(β)
+    # v_y = ||v⃗|| × sin(β)
+    # Puis on les ajoute à la position de la balle
+    x2 = x + balle_vitesse * cos(balle_direction * -PI/180 + PI/2) * dt
+    y2 = y + balle_vitesse * sin(balle_direction * -PI/180 + PI/2) * dt
+    fond.coords(balle, (x2, y2, x2, y2)) # on déplace la balle sur l'écran
+
+
+    # fond.coords(balle, (x2, y2, x2, y2)) # on déplace la balle
 
 temps_actuel = time.time()
 def boucle_de_jeu():
